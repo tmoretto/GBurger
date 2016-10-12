c8281e82a9a21e40523dce71b167c3c96e0971e8
Tiago Moretto <moretto.tiago@gmail.com>
2016-10-12 19:37:58 -0300

    adding readme

---
+++ readme.md
@@ -0,0 +1,1 @@
+Just my Ionic study guide project.
--- www/js/service.js
+++ www/js/service.js
@@ -1,18 +1,14 @@
 angular.module('starter').service('BurgerService', function($http){
-
     
     //var url_burgers = 'http://localhost:8080/burgers';
     var url_burgers = 'https://gibaburgerbackend.herokuapp.com/burgers';
-
     
     //var url_pimentas = 'http://localhost:8080/pimentas';
     var url_pimentas = 'https://gibaburgerbackend.herokuapp.com/pimentas';
-    
-    
+        
     //var url_post = 'http://localhost:8080/pedidos/create';
     var url_post = 'https://gibaburgerbackend.herokuapp.com/pedidos/create';
     
-
     var itensPedido = [];
 
     return {
